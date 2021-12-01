a8f24ab0c947245cb0dee749a2d8f15ae8125004
Bradley Donahue <donahue.bradley@gmail.com>
2021-12-01 12:57:56 -0500

    Use iterators better

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,38 +5,18 @@ fn main() {
 
 fn part1() {
     let input = include_str!("../input.txt");
-    let mut previous = None;
-    let mut count = 0;
-    for line in input.lines() {
-        let current = line.parse::<u32>().unwrap();
-        if previous == None {
-            previous = Some(current);
-            continue;
-        }
-        if current > previous.unwrap() {
-            count += 1;
-        }
-        previous = Some(current)
-    }
+    let lines: Vec<i32> = input.lines().map(|l| l.parse().unwrap()).collect();
+    let count = lines.windows(2).filter(|pair| pair[1] > pair[0]).count();
     println!("part1: {}", count);
 }
 
 fn part2() {
     let input = include_str!("../input.txt");
-    let lines: Vec<_> = input.lines().map(|l| l.parse::<u32>().unwrap()).collect();
+    let lines: Vec<i32> = input.lines().map(|l| l.parse().unwrap()).collect();
 
-    let mut previous = None;
-    let mut count = 0;
-    for window in lines.windows(3) {
-        let current_sum: u32 = window.iter().sum();
-        if previous == None {
-            previous = Some(current_sum);
-            continue;
-        }
-        if current_sum > previous.unwrap() {
-            count += 1;
-        }
-        previous = Some(current_sum);
-    }
+    // [a, b, c, d, e]
+    // In a sliding window of size 3 [a, b, c] and [b, c, d] we can take a window of size 4
+    // and just compare the 'a' and 'd' to check for an increase since items 'b' and 'c' stay the same.
+    let count = lines.windows(4).filter(|quad| quad[3] > quad[0]).count();
     println!("part2: {}", count);
 }
