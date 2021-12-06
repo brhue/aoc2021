f1ee8f23fa2ba7f18e57922a25d044e8e9848d23
Bradley Donahue <donahue.bradley@gmail.com>
2021-12-06 12:48:08 -0500

    Refactor solution

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -4,16 +4,8 @@ fn main() {
     println!("part2: {}", part2(input));
 }
 
-fn part1(input: &str) -> usize {
-    let mut fish =
-        input
-            .split(',')
-            .map(|f| f.parse::<u8>().unwrap())
-            .fold([0; 9], |mut fish_by_day, fish| {
-                fish_by_day[fish as usize] += 1;
-                fish_by_day
-            });
-    for _day in 0..80 {
+fn simulate(mut fish: [usize; 9], days: usize) -> usize {
+    for _day in 0..days {
         fish = fish
             .iter()
             .enumerate()
@@ -30,8 +22,19 @@ fn part1(input: &str) -> usize {
     fish.into_iter().sum()
 }
 
+fn part1(input: &str) -> usize {
+    let fish = input.split(',').map(|f| f.parse::<u8>().unwrap()).fold(
+        [0usize; 9],
+        |mut fish_by_day, fish| {
+            fish_by_day[fish as usize] += 1;
+            fish_by_day
+        },
+    );
+    simulate(fish, 80)
+}
+
 fn part2(input: &str) -> usize {
-    let mut fish =
+    let fish =
         input
             .split(',')
             .map(|f| f.parse::<u8>().unwrap())
@@ -39,21 +42,7 @@ fn part2(input: &str) -> usize {
                 fish_by_day[fish as usize] += 1;
                 fish_by_day
             });
-    for _day in 0..256 {
-        fish = fish
-            .iter()
-            .enumerate()
-            .fold([0; 9], |mut fish_by_day, (i, n)| {
-                if i == 0 {
-                    fish_by_day[6] += n;
-                    fish_by_day[8] += n;
-                } else {
-                    fish_by_day[i - 1] += n;
-                }
-                fish_by_day
-            });
-    }
-    fish.into_iter().sum()
+    simulate(fish, 256)
 }
 
 #[cfg(test)]
