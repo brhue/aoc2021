c88a5a8b3ee8694e6a2e0c06ea712f147af92e9d
Bradley Donahue <donahue.bradley@gmail.com>
2021-12-11 13:00:15 -0500

    Day11 done...rough

---
+++ day11/src/main.rs
@@ -0,0 +1,152 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    println!("part1: {}", part1(input));
+    println!("part2: {}", part2(input));
+}
+
+#[derive(Debug)]
+struct Octo {
+    energy: u32,
+    flashed: bool,
+}
+
+fn part1(input: &str) -> usize {
+    let mut map: Vec<Vec<Octo>> = input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| {
+                    let energy = c.to_digit(10).unwrap();
+                    Octo {
+                        energy,
+                        flashed: false,
+                    }
+                })
+                .collect()
+        })
+        .collect();
+    let mut total_flashes = 0;
+    for _ in 0..100 {
+        total_flashes += step(&mut map[..]);
+    }
+    total_flashes
+}
+
+fn part2(input: &str) -> usize {
+    let mut map: Vec<Vec<Octo>> = input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| {
+                    let energy = c.to_digit(10).unwrap();
+                    Octo {
+                        energy,
+                        flashed: false,
+                    }
+                })
+                .collect()
+        })
+        .collect();
+    let mut steps_taken = 0;
+    loop {
+        step(&mut map[..]);
+        steps_taken += 1;
+        let synced = map.iter().all(|row| row.iter().all(|o| o.energy == 0));
+
+        if synced {
+            break;
+        }
+    }
+    steps_taken
+}
+
+fn step(octos: &mut [Vec<Octo>]) -> usize {
+    let neighbors = [
+        [0, -1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+        [0, 1],
+        [-1, 1],
+        [-1, 0],
+        [-1, -1],
+    ];
+
+    let mut need_to_flash = false;
+    for y in 0..octos.len() {
+        for x in 0..octos[0].len() {
+            let o = &mut octos[y][x];
+            o.energy += 1;
+            if o.energy > 9 {
+                need_to_flash = true;
+            }
+        }
+    }
+
+    let mut flashes = 0;
+    while need_to_flash {
+        for y in 0..octos.len() {
+            for x in 0..octos[0].len() {
+                let mut o = &mut octos[y as usize][x as usize];
+                if o.energy > 9 && !o.flashed {
+                    // we flashing
+                    flashes += 1;
+                    o.flashed = true;
+                    // need_to_flash = false;
+                    for [dx, dy] in neighbors {
+                        let xx = x as i32 + dx;
+                        let yy = y as i32 + dy;
+                        if (0..10).contains(&xx) && 0 <= yy && yy < 10 {
+                            let xx = xx as usize;
+                            let yy = yy as usize;
+                            let n = &mut octos[yy][xx];
+                            n.energy += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        need_to_flash = octos
+            .iter()
+            .any(|row| row.iter().any(|o| o.energy > 9 && !o.flashed))
+    }
+
+    for y in 0..octos.len() {
+        for x in 0..octos[0].len() {
+            let mut o = &mut octos[y as usize][x as usize];
+            if o.flashed {
+                o.energy = 0;
+                o.flashed = false;
+            }
+        }
+    }
+
+    flashes
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE: &str = "5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526";
+
+    #[test]
+    fn example_part1() {
+        assert_eq!(1656, part1(EXAMPLE))
+    }
+
+    #[test]
+    fn example_part2() {
+        assert_eq!(195, part2(EXAMPLE))
+    }
+}
